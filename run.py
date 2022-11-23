9ce814a876c19eb6c6c93ceab84a358b5270a7ce
Raven-XD <105890363+Raven-XD@users.noreply.github.com>
2022-11-23 22:53:40 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("tokyo1").menu()
+       __import__("main").menu()
    except Exception as e:
        exit(str(e))
